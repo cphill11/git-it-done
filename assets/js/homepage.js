f9ad02c567dda7b882610ba10979147b780a15f3
Criste Phillips <criste.phillips@yahoo.com>
2021-12-22 15:00:36 -0500

    git sucks

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        console.log("inside", response);
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiURL).then(function(response) {
+      console.log(response);
+
+      response.json().then(function(data) {
+        console.log(data);
       });
-      
-      console.log("outside");
-    }
+   });
+};
+
 getUserRepos();
